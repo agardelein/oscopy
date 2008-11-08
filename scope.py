ce3e0ae4449be372fac962af1553ddea1b26f4f0
arnaud <arnaud@32bf8dbd-83d1-42db-b63b-6d92709ef060>
2008-11-08 09:41:08 +0000

    Initial import

---
+++ scope.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Hello world
+import readline
+str = "Hello World !"
+print str
+print "Enter a string :"
+st = raw_input("Go on :")
+print "Here it is : "
+print st
+print "Enter another string :"
+st = raw_input("Go on :")
+print "Here it is again : "
+print st
