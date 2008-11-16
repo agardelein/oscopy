ef8dc3eebef2d98f0c8503e1ac2d67e7001fabbd
arnaud <arnaud@32bf8dbd-83d1-42db-b63b-6d92709ef060>
2008-11-16 10:29:33 +0000

    Switch to class, added list, new, siglist

--- Cmd.py
+++ Cmd.py
@@ -1,9 +1,53 @@
 from Gnucap import *
 from Signal import *
+from Figure import *
 
-def load(args):
-    obj = Gnucap()
-    slist = obj.loadfile(args)
-    print args, ":"
-    for ns in slist:
-        print ns.name, "/", ns.domain
+
+class Cmds:
+    a = -1
+    figs = []
+    curfig = 0
+    sigs = []
+    files = []
+    
+    def __init__(self):
+        self.a = 0
+
+    # Load a file
+    def load(self, args):
+        for s in self.files:
+            if s == args:
+                print "Already here"
+                return
+        obj = Gnucap() # for now only Gnucap is supported
+        self.sigs.extend(obj.loadfile(args))
+        print args, ":"
+        for ns in self.sigs:
+            print ns.name, "/", ns.domain
+        self.files.append(args)
+
+    # Plot signals
+    def plot(self, args):
+        if self.sigs == []:
+            print "No signal loaded"
+            return
+        self.a = self.a + 1
+        print "plot " + str(self.a)
+        if self.figs == []:
+            self.new()
+
+    # List of figures
+    def list(self, args):
+        print "list"
+        for f in self.figs:
+            f.list()
+
+    # Create a new figure, set it as current
+    def new(self):
+        print "newfig"
+        self.curfig = Figure()
+        self.figs.append(self.curfig)
+
+    def siglist(self, args):
+        for s in self.sigs:
+            print s
