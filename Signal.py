39632b85f2d5be225e53fd0677d096771ebfd4a2
Arnaud Gardelein <agard@nostalgia-for-infinity.(none)>
2008-12-07 17:44:41 +0100

    More comments, again

--- Signal.py
+++ Signal.py
@@ -1,12 +1,34 @@
+""" Signal list,
 
+A signal is the common point between the reader and the graph objects,
+i.e. the file and the plot.
+
+A signal contains a list of points, with an identifier, a reference (abscisse) and the original reader object.
+
+The identifier is the signal name, as presented to the user.
+The signal contains the ordinate values, while the abscisses are contained
+into the reference signal.
+The reference signal has its ref set to None.
+
+The reader object is stored into the signal, if any use.
+
+Class Signal -- Contains the signal points and other information
+   __init(name, reader)__
+      Create an empty signal.
+      If given, set the name and the reader.
+
+   __str__()
+      Returns a string with the signal name, the reference name
+      and the reader name.
+"""
 
 # Signals class
 class Signal:
     def __init__(self, name = "", reader = None):
-        self.name = name         # Identifier
-        self.reader = reader     # Reader object
         self.pts = []            # Data points
+        self.name = name         # Identifier
         self.ref = None          # Reference signal
+        self.reader = reader     # Reader object
 
     def __str__(self):
         a = self.name + " / " + (self.ref.name) + " (" + str(self.reader) + ") "
