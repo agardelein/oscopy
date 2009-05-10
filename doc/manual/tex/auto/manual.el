50de0222c9088fd22ce15293213adba374019a2c
Arnaud Gardelein <agard@nostalgia-for-infinity.(none)>
2009-05-10 17:35:25 +0200

    Doc: added Introduction, user and data interaction

--- doc/manual/tex/auto/manual.el
+++ doc/manual/tex/auto/manual.el
@@ -2,13 +2,26 @@
  (lambda ()
     (LaTeX-add-labels
      "sec:intro"
-     "sec:obj"
      "sec:itf"
+     "fig:itf"
+     "sec:user"
+     "sec:data"
+     "sec:fileinter"
+     "fig:fileinter"
+     "sec:viewinter"
+     "fig:viewinter"
      "sec:sigs"
+     "sec:readers"
+     "sec:writers"
      "sec:graphs"
-     "sec:figs")
+     "sec:figs"
+     "sec:curs"
+     "sec:ext")
     (TeX-run-style-hooks
+     "graphicx"
      "a4wide"
+     "babel"
+     "english"
      "latex2e"
      "art11"
      "article"
