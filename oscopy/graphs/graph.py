a1193c63136eeb1845fd8fc0d92b41ddfa472be9
Arnaud Gardelein <agard@nostalgia-for-infinity.(none)>
2009-07-24 22:07:56 +0200

    graph: plot the signals in insert()

--- oscopy/graphs/graph.py
+++ oscopy/graphs/graph.py
@@ -155,6 +155,7 @@ class Graph(object):
                 del self._sigs[sn]
                 ## ipython: To remove the line from the plot:
                 ## line.remove() and then plot() <- matplotlib plot
+                self._signals2lines[sn].remove()
                 del self._signals2lines[sn]
         return len(self._sigs)
 
