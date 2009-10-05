8e3f7b2018f60cfd9efc5812eb4d2f7a2dabf620
Arnaud Gardelein <agard@nostalgia-for-infinity.(none)>
2009-10-05 23:30:49 +0200

    ui: support for running netlister, simulate and update results

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 import gobject
 import gtk
 import signal
+import commands
 
 from oscopy.readers.detect_reader import DetectReader
 from oscopy import Figure
@@ -24,6 +25,7 @@ class App(object):
         <menuitem action="Add file"/>
         <menuitem action="Update files"/>
         <menuitem action="New Math Signal..."/>
+        <menuitem action="Run netlister and simulate..."/>
         <menuitem action="Quit"/>
       </menu>
     </menubar>
@@ -113,6 +115,9 @@ class App(object):
              self._action_update),
             ("New Math Signal...", gtk.STOCK_NEW, '_New Math Signal', None,
              None, self._action_new_math),
+            ("Run netlister and simulate...", gtk.STOCK_MEDIA_FORWARD,\
+                 "_Run netlister and simulate...", None, None,\
+                 self._action_netlist_and_simulate),
             ('Quit', gtk.STOCK_QUIT, '_Quit', None, None,
              self._action_quit),
             ]
@@ -480,7 +485,48 @@ class App(object):
 
     def update_from_usr1(self):
         self._ctxt.update()
-        
+
+    def _action_netlist_and_simulate(self, action):
+        dialog = gtk.Dialog("Run netlister and simulate",\
+                                buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,\
+                                             gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+        vbox_netl = gtk.VBox()
+        ckbutton_netl = gtk.CheckButton("Run netlister")
+        ckbutton_netl.set_active(True)
+        vbox_netl.pack_start(ckbutton_netl)
+        entry_netl = gtk.Entry()
+        entry_netl.set_text("gnetlist -s -o demo.cir -g spice-sdb demo.sch")
+        vbox_netl.pack_start(entry_netl)
+        dialog.vbox.pack_start(vbox_netl)
+        vbox_netl = gtk.VBox()
+
+        vbox_sim = gtk.VBox()
+        ckbutton_sim = gtk.CheckButton("Run simulator")
+        ckbutton_sim.set_active(True)
+        vbox_sim.pack_start(ckbutton_sim)
+        entry_sim = gtk.Entry()
+        entry_sim.set_text("gnucap -b demo.cir")
+        vbox_sim.pack_start(entry_sim)
+        dialog.vbox.pack_start(vbox_sim)
+        ckbutton_upd = gtk.CheckButton("Update readers")
+        ckbutton_upd.set_active(True)
+        dialog.vbox.pack_start(ckbutton_upd)
+        dialog.show_all()
+        resp = dialog.run()
+        if resp == gtk.RESPONSE_ACCEPT:
+            if ckbutton_netl.get_active():
+                res = commands.getstatusoutput(entry_netl.get_text())
+                if res[0]:
+                    report_error(self._mainwindow, res[1])
+                print res[1]
+            if ckbutton_sim.get_active():
+                res = commands.getstatusoutput(entry_sim.get_text())
+                if res[0]:
+                    report_error(self._mainwindow, res[1])
+                print res[1]
+            if ckbutton_upd.get_active():
+                self._ctxt.update()
+        dialog.destroy()
 
 def usr1_handler(signum, frame):
     app.update_from_usr1()
