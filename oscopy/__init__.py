7556d74fafd07f584ac230107b143174293f98cf
Adrian <pardo@onyx.(none)>
2010-05-05 09:23:02 -0300

    Basic stuff for i18n

--- oscopy/__init__.py
+++ oscopy/__init__.py
@@ -5,3 +5,17 @@ from context import Context
 from app import OscopyApp
 from readers.reader import ReadError
 from graphs import factors_to_names, abbrevs_to_factors, names_to_factors
+
+
+
+GETTEXT_DOMAIN = 'oscopy'
+import gettext
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
+
+gettext.bindtextdomain(GETTEXT_DOMAIN)
+gettext.textdomain(GETTEXT_DOMAIN)
+
+import __builtin__
+__builtin__._ = gettext.gettext
