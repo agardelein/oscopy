1ec2bbfc9dfe8e2d82968503c1a97e2751f830d1
Arnaud Gardelein <agardelein@yahoo.fr>
2011-01-07 11:50:40 +0100

    Update autotools files
    As some files where moves and some are not used anymore, update configure.ac and the Makefile.am accordingly

--- oscopy/__init__.py
+++ oscopy/__init__.py
@@ -2,7 +2,6 @@
 from signal import Signal
 from figure import Figure
 from context import Context
-from app import OscopyApp
 from readers.reader import ReadError
 from graphs import factors_to_names, abbrevs_to_factors, names_to_factors
 import gobject
