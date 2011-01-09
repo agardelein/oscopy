b5b68e6ef472396d2963c2a9e593d935f402e3ea
Arnaud Gardelein <agardelein@yahoo.fr>
2011-01-09 15:33:46 +0100

    Rename executable to oscopy_app, FIXED: Put ipythonrc-oscopy in /home/arnaud/.ipython if not existing.
    As oscopy name is already existing in the source directory, executable create from ioscopy_app.py cannot be named 'oscopy' so we use the name 'oscopy_app'.
    
    IPython require the file ipythonrc-oscopy to be present in /home/arnaud/.ipython. We test if this file exist to copy it if needed before launching ipython. ipythonrc-oscopy is located in @datarootdir@/oscopy.

--- ioscopy_app.py
+++ ioscopy_app.py
@@ -1,1 +1,6 @@
+#!/bin/sh
+#-*-Shell-*-
+if [[ ! -e ${HOME}/.ipython/ipythonrc-oscopy ]]; then
+    cp @datarootdir@/oscopy/ipythonrc-oscopy ${HOME}/.ipython
+fi
 ipython -pylab -profile oscopy -noconfirm_exit -nobanner
