agardelein/oscopy

commit 99a998095d910af48143ad385a82a1e811e0c91f

Check if __IPYTHON__ is in globals() rather than testing its value
This fix a bug when oscopy is imported from python

oscopy/__init__.py
@@ -18,9 +18,8 @@ gettext.textdomain(GETTEXT_DOMAIN)
 import __builtin__
 __builtin__._ = gettext.gettext
 
-import ioscopy
-
 gobject.type_register(signal.Signal)
 
-if __IPYTHON__:
+if hasattr(globals(), '__IPYTHON__'):
+    import ioscopy
     ioscopy.init()

commit 27977a4c9c4e3923bc4784c6788d1410dd335e7c

FIXED: IPython check on oscopy import Use a exception scheme as reported in http://ipython.scipy.org/doc/stable/html/interactive/reference.html#system-command-aliases. This shall fixe issue introduced by 99a99809 on oscopy import from IPython

oscopy/__init__.py
@@ -20,6 +20,10 @@ __builtin__._ = gettext.gettext
 
 gobject.type_register(signal.Signal)
 
-if hasattr(globals(), '__IPYTHON__'):
+try:
+    __IPYTHON__
+except NameError:
+    pass
+else:
     import ioscopy
     ioscopy.init()