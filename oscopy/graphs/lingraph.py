27b9b609c02252f24d775ded81b17d7b7af105a0
Arnaud Gardelein <agardelein@yahoo.fr>
2011-05-08 20:06:35 +0200

    lingraph.py: update comments

--- oscopy/graphs/lingraph.py
+++ oscopy/graphs/lingraph.py
@@ -1,27 +1,26 @@
-""" A graph with linear scale on X and Y axis
-
-Class LinGraph -- Draw graph with linear scale on X and Y axis
-
-   methods:
-   type()
-      Return the mode of the graph "linear"
-
-Class XYGraph -- Draw a xy graph using signals pairs
-   methods:
-   type()
-      Return the mode of the graph "linear"
-"""
-
 from graph import Graph
 
 class LinGraph(Graph):
+    """ Class LinGraph -- Draw graph with linear scale on X and Y axis
+    """
     @property
     def type(self):
         """ Return 'linear', the type of the graph
+
+        Parameter
+        ---------
+        None
+
+        Returns
+        -------
+        string:
+        The type of the graph
         """
         return "linear"
 
 # class XYGraph(Graph.Graph):
+#     """ Class XYGraph -- Draw a xy graph using signals pairs
+#     """
 #     def get_type(self):
 #         """ Return 'xy', the type of the graph
 #         """
